434719f2286ea6c1f9c8f02a2846e27f3f6ad934
Simon J Stuart <lakraven@gmail.com>
2026-08-26 19:31:32 +0200

    #57 normalize RTTI-free Event type keys without warnings

--- src/ESPressio_EventTypeKey.hpp
+++ src/ESPressio_EventTypeKey.hpp
@@ -7,12 +7,21 @@ namespace Event {
 
 using EventTypeKey = const void*;
 
+namespace Detail {
+
+template<typename T>
+EventTypeKey EventTypeKeyStorage() noexcept {
+    static const unsigned char token = 0;
+    return static_cast<EventTypeKey>(&token);
+}
+
+} // namespace Detail
+
 template<typename T>
 EventTypeKey EventTypeKeyOf() noexcept {
     using WithoutReference = typename std::remove_reference<T>::type;
     using Normalized = typename std::remove_cv<WithoutReference>::type;
-    static const unsigned char token = 0;
-    return static_cast<EventTypeKey>(&token);
+    return Detail::EventTypeKeyStorage<Normalized>();
 }
 
 } // namespace Event

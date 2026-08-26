38c34496d2a45e187d718ce6489a890fe243f875
Simon J Stuart <lakraven@gmail.com>
2026-08-26 20:40:53 +0200

    fix(#57): condition legacy serializable GC bridge

--- src/ESPressio_ThreadEventBridges_Serializable.hpp
+++ src/ESPressio_ThreadEventBridges_Serializable.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
 #include "ESPressio_ThreadManagerEventBridge_Serializable.hpp"
-#include "ESPressio_ThreadGarbageCollectorEventBridge_Serializable.hpp"
+
+#if __has_include(<ESPressio_ThreadGarbageCollector.hpp>)
+    #include "ESPressio_ThreadGarbageCollectorEventBridge_Serializable.hpp"
+#endif
+
 #include "ESPressio_ThreadTerminationDispatcherEventBridge_Serializable.hpp"

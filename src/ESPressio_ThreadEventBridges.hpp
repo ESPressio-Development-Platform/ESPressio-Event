1429edd7bd1c26334b8948588896d7cf6a0ce585
Simon J Stuart <lakraven@gmail.com>
2026-08-26 21:18:37 +0200

    Remove obsolete Thread GC bridge export

--- src/ESPressio_ThreadEventBridges.hpp
+++ src/ESPressio_ThreadEventBridges.hpp
@@ -1,9 +1,4 @@
 #pragma once
 
 #include "ESPressio_ThreadManagerEventBridge.hpp"
-
-#if __has_include(<ESPressio_ThreadGarbageCollector.hpp>)
-    #include "ESPressio_ThreadGarbageCollectorEventBridge.hpp"
-#endif
-
 #include "ESPressio_ThreadTerminationDispatcherEventBridge.hpp"

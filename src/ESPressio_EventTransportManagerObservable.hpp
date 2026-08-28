3b3187284e6dd488db772587ded73ed4785d41d9
Simon J Stuart <lakraven@gmail.com>
2026-08-28 16:56:00 +0200

    docs(event): document transport manager observable (#61)

--- src/ESPressio_EventTransportManagerObservable.hpp
+++ src/ESPressio_EventTransportManagerObservable.hpp
@@ -6,8 +6,12 @@
 
 namespace ESPressio::Event {
 
+/// <summary>Thread-safe observable used internally to fan out Event transport-manager notifications.</summary>
 class EventTransportManagerObservable final : public Observable::ThreadSafeObservable {
 public:
+    /// <summary>Invokes a callback for each registered Event transport-manager observer.</summary>
+    /// <typeparam name="TCallback">Callable accepting an IEventTransportManagerObserver pointer.</typeparam>
+    /// <remarks>Exceptions raised by individual observers are contained so one observer cannot interrupt manager state transitions or other notifications.</remarks>
     template<typename TCallback>
     void Notify(TCallback&& callback) {
         ExecuteNotification([&](NotificationContext& notification) {
@@ -19,6 +23,7 @@ public:
     }
 };
 
+/// <summary>Creates a shared Event transport-manager observable.</summary>
 inline std::shared_ptr<EventTransportManagerObservable> CreateEventTransportManagerObservable() {
     return std::make_shared<EventTransportManagerObservable>();
 }
